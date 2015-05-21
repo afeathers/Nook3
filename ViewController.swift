ae648b2e4a3a5df9fa4c4eb0fee9da7ed576a67c
afeathers <alex.tjfads@gmail.com>
2015-05-21 16:50:38 -0400

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -2,13 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var timer = NSTimer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
         
     }
 
